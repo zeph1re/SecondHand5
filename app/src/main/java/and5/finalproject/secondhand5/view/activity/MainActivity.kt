81ce608eb2779016f7324ccdce68825c9b9fff4c
Riandika <riandikasp@gmail.com>
2022-06-14 21:52:15 +0700

    SplashScreen, Login - Andika

---
+++ app/src/main/java/and5/finalproject/secondhand5/view/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package and5.finalproject.secondhand5.view.activity
+
+import and5.finalproject.secondhand5.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
