5f4aceee2f05399ebabb9f48d889f09f51c15734
dimassekti <dimas.adji@binus.ac.id>
2022-06-14 22:30:40 +0700

    Register @dimassekti

---
+++ app/src/main/java/and5/finalproject/secondhand5/view/fragment/Register.kt
@@ -0,0 +1,23 @@
+package and5.finalproject.secondhand5.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import and5.finalproject.secondhand5.R
+
+
+class Register : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+
+}
