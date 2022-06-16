8e8556e469b3e3ef147f3816d2fd88eee53848bb
zeph1re <arlinnog1122@gmail.com>
2022-06-16 20:32:16 +0700

    .

---
+++ app/src/main/java/and5/finalproject/secondhand5/view/fragment/Account.kt
@@ -0,0 +1,25 @@
+package and5.finalproject.secondhand5.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import and5.finalproject.secondhand5.R
+
+
+class Account : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
