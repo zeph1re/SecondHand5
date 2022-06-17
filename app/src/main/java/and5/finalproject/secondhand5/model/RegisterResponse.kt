282d7fd463c431a51796d8e1de06377391d796c0
zeph1re <arlinnog1122@gmail.com>
2022-06-17 20:53:21 +0700

    Add Detail Penawar

--- app/src/main/java/and5/finalproject/secondhand5/model/RegisterResponse.kt
+++ app/src/main/java/and5/finalproject/secondhand5/model/RegisterResponse.kt
@@ -5,7 +5,7 @@ data class RegisterResponse(
     val full_name: String,
     val email: String,
     val password : String,
-    val phone_number : String,
+    val phone_number : Int,
     val address : String,
     val image_url : String,
     val createAt : String,
