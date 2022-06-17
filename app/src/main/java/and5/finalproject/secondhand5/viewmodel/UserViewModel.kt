66f64577d4f6719762c5df9fed9e486c4cd877ef
Riandika <riandikasp@gmail.com>
2022-06-17 20:20:11 +0700

    pull

--- app/src/main/java/and5/finalproject/secondhand5/viewmodel/UserViewModel.kt
+++ app/src/main/java/and5/finalproject/secondhand5/viewmodel/UserViewModel.kt
@@ -3,6 +3,7 @@ package and5.finalproject.secondhand5.viewmodel
 import and5.finalproject.secondhand5.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 
+
 @HiltViewModel
 class UserViewModel constructor (val userRepo : UserRepository){
 
