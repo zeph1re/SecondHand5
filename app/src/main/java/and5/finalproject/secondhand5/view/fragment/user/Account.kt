c9d52a9238ed4d3e110a2d71c9ef475b70a65357
zeph1re <arlinnog1122@gmail.com>
2022-06-21 19:45:07 +0700

    Update Account.kt @zeph1re

--- app/src/main/java/and5/finalproject/secondhand5/view/fragment/user/Account.kt
+++ app/src/main/java/and5/finalproject/secondhand5/view/fragment/user/Account.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import and5.finalproject.secondhand5.R
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_account.*
 
 
 class Account : Fragment() {
@@ -20,6 +22,20 @@ class Account : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        ubah_akun_card.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_account_to_profile)
+        }
+
+        setting_card.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_account_to_home)
+        }
+
+        logout_card.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_account_to_home)
+        }
+
+
 
 
 
